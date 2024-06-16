d344ad107a90e0ef235680d711f674bb6f7dafee
lsh <tmdgns4029@naver.com>
2024-06-16 22:18:21 +0900

    플라이웨이 스키마 V1 수정

--- src/main/resources/db/migration/V1__Initial_schema.sql
+++ src/main/resources/db/migration/V1__Initial_schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE book(
     title varchar(255) NOT NULL ,
     author varchar(255) NOT NULL ,
     price float8 NOT NULL ,
-    create_date timestamp NOT NULL ,
+    created_date timestamp NOT NULL ,
     last_modified_date timestamp NOT NULL ,
     version integer NOT NULL
 );
